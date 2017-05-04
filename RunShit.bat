32029d5350a5ef7942498871f1749ad5156fe3df
emartinez1621 <emartinez@crestfinancial.com>
2017-05-04 17:06:25 -0600

    Added progress and error logging.

--- RunShit.bat
+++ RunShit.bat
@@ -1,3 +1,2 @@
 @echo off
-if NOT ["%errorlevel%"]==["0"] pause
 powershell.exe -executionPolicy remotesigned -File scripts/CSS-Gmod-Assets.ps1
--- Scripts/CSS-Gmod-Assets.ps1
+++ Scripts/CSS-Gmod-Assets.ps1
@@ -1,11 +1,11 @@
 ﻿$wshell = New-Object -ComObject Wscript.Shell
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$errorPath = [Environment]::GetFolderPath("Desktop") + "\errors.txt"
 $DownloadPath = "$env:UserProfile\Downloads"
 $GmodFilePath = "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod"
 $CSSTexturesURL = "http://files3.fragplays.com/CSS%20Game%20Content.zip"
-$CSSTexturesOutput = "$DownloadPath\CSS Game Content.zip"
+$CSSTexturesOutput = "$DownloadPath\CSS_Game_Content.zip"
 $CSSMapsURL = "http://files3.fragplays.com/CSS%20Maps.zip"
-$CSSMapsOutput = "$DownloadPath\CSS Maps.zip"
+$CSSMapsOutput = "$DownloadPath\CSS_Maps.zip"
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
@@ -15,15 +15,55 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-$wshell.Popup("Downloading CSS Textures",2,"Message",0);
-(New-Object System.Net.WebClient).DownloadFile($CSSTexturesURL, $CSSTexturesOutput)
-$wshell.Popup("Downloading CSS Maps",2,"Message",0);
-(New-Object System.Net.WebClient).DownloadFile($CSSMapsURL, $CSSMapsOutput)
+function DownloadFile($url, $targetFile)
+{
+   $uri = New-Object "System.Uri" "$url"
+   $request = [System.Net.HttpWebRequest]::Create($uri)
+   $request.set_Timeout(15000) #15 second timeout
+   $response = $request.GetResponse()
+   $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
+   $responseStream = $response.GetResponseStream()
+   $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+   $buffer = new-object byte[] 10KB
+   $count = $responseStream.Read($buffer,0,$buffer.length)
+   $downloadedBytes = $count
 
-$wshell.Popup("Unzipping the files",2,"Message",0);
-Unzip $CSSMapsOutput $DownloadPath
-Unzip $CSSTexturesOutput $DownloadPath
+   while ($count -gt 0)
+   {
+       $targetStream.Write($buffer, 0, $count)
+       $count = $responseStream.Read($buffer,0,$buffer.length)
+       $downloadedBytes = $downloadedBytes + $count
+       Write-Progress -activity "Downloading file '$($url.split('/') | Select -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
+   }
 
+   Write-Progress -activity "Finished downloading file '$($url.split('/') | Select -Last 1)'"
+   $targetStream.Flush()
+   $targetStream.Close()
+   $targetStream.Dispose()
+   $responseStream.Dispose()
+}
+
+
+try
+{
+    $wshell.Popup("Downloading CSS Textures",2,"Message",0);
+    DownloadFile $CSSTexturesURL $CSSTexturesOutput
+    $wshell.Popup("Downloading CSS Maps",2,"Message",0);
+    DownloadFile $CSSMapsURL $CSSMapsOutput 
+
+    $wshell.Popup("Unzipping the files",2,"Message",0);
+    Unzip $CSSMapsOutput $DownloadPath
+    Unzip $CSSTexturesOutput $DownloadPath  
+
+}
+catch
+{
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = "Downloading files"
+    $ErrorMessage | Out-File -Encoding Ascii -Append $errorPath
+    $wshell.Popup("Something failed. Check the error log at $($errorPath)",5,"Message",0);
+    Break   
+}
 $wshell.Popup("Moving the files to the right folders",3,"Message",0);
 Move-Item $DownloadPath\CSS_Game_Content $GmodFilePath\addons
 Move-Item $DownloadPath\CSS_Maps\maps\graphs $GmodFilePath\maps
@@ -34,4 +74,16 @@ Remove-Item $CSSTexturesOutput
 Remove-Item $CSSMapsOutput
 Remove-Item $DownloadPath\CSS_Maps -Force -Recurse
 
-$wshell.Popup("All done! Have a good day!",5,"Message",0);
+
+if($Error)
+{
+    $Error | Out-File -Encoding Ascii -Append $errorPath
+    $wshell.Popup("Something failed. Check the error log at $($errorPath)",5,"Message",0);
+    Break   
+}
+else 
+{
+    $wshell.Popup("All done! Have a good day!",5,"Message",0);      
+}
+
+
